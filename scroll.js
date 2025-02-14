50c2d8b9b1ed8e2f879b1d36d2ca4f935d25d212
Pablo <Pablo@DESKTOP-839L2V2>
2025-02-14 18:15:28 -0300

    my portfolio

---
+++ scroll.js
@@ -0,0 +1,6 @@
+﻿function scrollToElement(id) {
+    const element = document.getElementById(id);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+}
